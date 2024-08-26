dda8679305990e3bf6a26fc958945cfb48feda64
Mayank Agrawal <mayankagrawal.main@gmail.com>
2024-08-26 17:01:00 +0530

    Define and use custom properties

--- src/main/java/com/caizin/springboot/demo/myspringapp/controllers/AppRestController.java
+++ src/main/java/com/caizin/springboot/demo/myspringapp/controllers/AppRestController.java
@@ -1,12 +1,24 @@
 package com.caizin.springboot.demo.myspringapp.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppRestController {
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello World";
+    }
+
+    @GetMapping("/team-info")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team: " + teamName;
     }
 }
