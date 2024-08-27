43c65d669617e70084b531a3e304cabd6e01b890
Mayank Agrawal <mayankagrawal.main@gmail.com>
2024-08-28 00:19:12 +0530

    Check bean scope - singleton by default

--- src/main/java/com/caizin/springboot/demo/myspringapp/controllers/CoachController.java
+++ src/main/java/com/caizin/springboot/demo/myspringapp/controllers/CoachController.java
@@ -9,15 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CoachController {
     private final Coach myCoach;
+    private final Coach anotherCoach;
 
     @Autowired // Autowired annotation optional if only one constructor
-    public CoachController(@Qualifier("cricketCoach") Coach myCoach) {
+    public CoachController(
+            @Qualifier("cricketCoach") Coach myCoach,
+            @Qualifier("cricketCoach") Coach anotherCoach
+    ) {
         System.out.println("In constructor: " + getClass().getSimpleName());
         this.myCoach = myCoach;
+        this.anotherCoach = anotherCoach;
     }
 
     @GetMapping("/daily-workout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
     }
 }
