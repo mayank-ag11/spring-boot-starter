7f345c1d7459fbd228d82f050747927f6becb387
Mayank Agrawal <mayankagrawal.main@gmail.com>
2024-08-27 21:16:05 +0530

    Setter Injection

---
+++ src/main/java/com/caizin/springboot/demo/myspringapp/controllers/CoachController2.java
@@ -0,0 +1,21 @@
+package com.caizin.springboot.demo.myspringapp.controllers;
+
+import com.caizin.springboot.demo.myspringapp.interfaces.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoachController2 {
+    private Coach myCoach;
+
+    @Autowired // the method name can be anything
+    public void setCoach(Coach myCoach) {
+        this.myCoach = myCoach;
+    }
+
+    @GetMapping("/daily-workout-2")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout() + " (V2)";
+    }
+}
