13f9ac687347b7513ec975cc6a2f444d22da0b79
Mayank Agrawal <mayankagrawal.main@gmail.com>
2024-08-28 00:26:32 +0530

    Clean up code - Remove extra coach

--- src/main/java/com/caizin/springboot/demo/myspringapp/controllers/CoachController.java
+++ src/main/java/com/caizin/springboot/demo/myspringapp/controllers/CoachController.java
@@ -9,25 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CoachController {
     private final Coach myCoach;
-    private final Coach anotherCoach;
 
     @Autowired // Autowired annotation optional if only one constructor
-    public CoachController(
-            @Qualifier("cricketCoach") Coach myCoach,
-            @Qualifier("cricketCoach") Coach anotherCoach
-    ) {
+    public CoachController(@Qualifier("trackCoach") Coach myCoach) {
         System.out.println("In constructor: " + getClass().getSimpleName());
         this.myCoach = myCoach;
-        this.anotherCoach = anotherCoach;
     }
 
     @GetMapping("/daily-workout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
-    }
-
-    @GetMapping("/check")
-    public String check() {
-        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
     }
 }
