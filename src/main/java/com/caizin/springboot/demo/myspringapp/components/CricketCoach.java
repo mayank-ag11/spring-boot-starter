ef2845a982027a8ac64679960ff084a3f30d5f57
Mayank Agrawal <mayankagrawal.main@gmail.com>
2024-08-28 00:21:00 +0530

    Create separate instances using Prototype scope

--- src/main/java/com/caizin/springboot/demo/myspringapp/components/CricketCoach.java
+++ src/main/java/com/caizin/springboot/demo/myspringapp/components/CricketCoach.java
@@ -1,9 +1,12 @@
 package com.caizin.springboot.demo.myspringapp.components;
 
 import com.caizin.springboot.demo.myspringapp.interfaces.Coach;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
     public CricketCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
