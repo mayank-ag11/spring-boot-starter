a2051be322743d4db92be705df14d1521b797b0f
Mayank Agrawal <mayankagrawal.main@gmail.com>
2024-08-27 23:57:24 +0530

    Use Primary to resolve multiple components

--- src/main/java/com/caizin/springboot/demo/myspringapp/components/TennisCoach.java
+++ src/main/java/com/caizin/springboot/demo/myspringapp/components/TennisCoach.java
@@ -1,9 +1,11 @@
 package com.caizin.springboot.demo.myspringapp.components;
 
 import com.caizin.springboot.demo.myspringapp.interfaces.Coach;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary // Qualifier has higher precedence
 public class TennisCoach implements Coach {
     @Override
     public String getDailyWorkout() {
--- src/main/java/com/caizin/springboot/demo/myspringapp/controllers/CoachController2.java
+++ src/main/java/com/caizin/springboot/demo/myspringapp/controllers/CoachController2.java
@@ -2,7 +2,6 @@ package com.caizin.springboot.demo.myspringapp.controllers;
 
 import com.caizin.springboot.demo.myspringapp.interfaces.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +10,7 @@ public class CoachController2 {
     private Coach myCoach;
 
     @Autowired // the method name can be anything
-    public void setCoach(@Qualifier("tennisCoach") Coach myCoach) {
+    public void setCoach(Coach myCoach) {
         this.myCoach = myCoach;
     }
 
