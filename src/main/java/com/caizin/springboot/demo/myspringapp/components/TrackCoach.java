142cb3276451a8083532f2436944934ed53832aa
Mayank Agrawal <mayankagrawal.main@gmail.com>
2024-08-28 08:53:48 +0530

    Bean lifecycle methods

--- src/main/java/com/caizin/springboot/demo/myspringapp/components/TrackCoach.java
+++ src/main/java/com/caizin/springboot/demo/myspringapp/components/TrackCoach.java
@@ -1,6 +1,8 @@
 package com.caizin.springboot.demo.myspringapp.components;
 
 import com.caizin.springboot.demo.myspringapp.interfaces.Coach;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +11,16 @@ import org.springframework.stereotype.Component;
 public class TrackCoach implements Coach {
     public TrackCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @PostConstruct
+    public void onInit() {
+        System.out.println("In onInit: " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void onDestroy() {
+        System.out.println("In onDestroy: " + getClass().getSimpleName());
     }
 
     @Override
