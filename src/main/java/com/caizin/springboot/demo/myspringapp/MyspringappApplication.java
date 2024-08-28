f04958e9492f3942d45a3b558a3c611e32d179f1
Mayank Agrawal <mayankagrawal.main@gmail.com>
2024-08-28 12:39:17 +0530

    Query all the students

--- src/main/java/com/caizin/springboot/demo/myspringapp/MyspringappApplication.java
+++ src/main/java/com/caizin/springboot/demo/myspringapp/MyspringappApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 //@SpringBootApplication(
 //		scanBasePackages = {"com.caizin.springboot.demo.myspringapp",
 //							"com.caizin.springboot.demo.testscan"}
@@ -25,8 +27,17 @@ public class MyspringappApplication {
 			// createStudent(studentDAO);
 			// createMultipleStudents(studentDAO);
 
-			readStudent(studentDAO);
+			// readStudent(studentDAO);
+			queryForStudents(studentDAO);
 		};
+	}
+
+	private void queryForStudents(StudentDAO studentDAO) {
+		List<Student> students = studentDAO.findAll();
+
+		for(Student student : students) {
+			System.out.println(student);
+		}
 	}
 
 	private void readStudent(StudentDAO studentDAO) {
--- src/main/java/com/caizin/springboot/demo/myspringapp/dao/StudentDAO.java
+++ src/main/java/com/caizin/springboot/demo/myspringapp/dao/StudentDAO.java
@@ -2,8 +2,12 @@ package com.caizin.springboot.demo.myspringapp.dao;
 
 import com.caizin.springboot.demo.myspringapp.entities.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
     void save(Student student);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
 }
