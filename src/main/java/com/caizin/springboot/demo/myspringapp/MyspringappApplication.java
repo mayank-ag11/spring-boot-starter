9340fcaf4efefb2454c1c36ca9a92998e2cd2e58
Mayank Agrawal <mayankagrawal.main@gmail.com>
2024-08-28 12:22:09 +0530

    Create and save students

--- src/main/java/com/caizin/springboot/demo/myspringapp/MyspringappApplication.java
+++ src/main/java/com/caizin/springboot/demo/myspringapp/MyspringappApplication.java
@@ -1,5 +1,7 @@
 package com.caizin.springboot.demo.myspringapp;
 
+import com.caizin.springboot.demo.myspringapp.dao.StudentDAO;
+import com.caizin.springboot.demo.myspringapp.entities.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,9 +20,32 @@ public class MyspringappApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello world");
+			// createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		System.out.println("Creating new student object...");
+		Student student = new Student("Mayank", "Agrawal", "mayank@gmail.com");
+
+		System.out.println("Saving the student...");
+		studentDAO.save(student);
+
+		System.out.println("Saved student. Generated id: " + student.getId());
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		System.out.println("Creating 3 student objects...");
+		Student student1 = new Student("John", "Doe", "johndoe@gmail.com");
+		Student student2 = new Student("Jane", "Doe", "janedoe@gmail.com");
+		Student student3 = new Student("Mary", "Public", "mary@gmail.com");
+
+		System.out.println("Saving the students...");
+		studentDAO.save(student1);
+		studentDAO.save(student2);
+		studentDAO.save(student3);
 	}
 }
