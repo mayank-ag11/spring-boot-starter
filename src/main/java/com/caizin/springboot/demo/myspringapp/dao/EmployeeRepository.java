272ea4cfc5505d854a286059c3fcc46bf5aaa190
Mayank Agrawal <mayankagrawal.main@gmail.com>
2024-09-01 17:31:26 +0530

    Config properties for code less APIs

--- src/main/java/com/caizin/springboot/demo/myspringapp/dao/EmployeeRepository.java
+++ src/main/java/com/caizin/springboot/demo/myspringapp/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.caizin.springboot.demo.myspringapp.dao;
 
 import com.caizin.springboot.demo.myspringapp.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// @RepositoryRestResource(path = "members") // change the default API path
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
